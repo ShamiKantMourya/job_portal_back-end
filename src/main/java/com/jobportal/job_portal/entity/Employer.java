25e6495b8f0f42bde7b855876da9c4a8ce58c4cf
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-20 21:13:57 +0530

    employer constructor

--- src/main/java/com/jobportal/job_portal/entity/Employer.java
+++ src/main/java/com/jobportal/job_portal/entity/Employer.java
@@ -1,5 +1,29 @@
 package com.jobportal.job_portal.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Employer {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    public String companyName;
+    public String email;
+    public String password;
+
+    public Employer(){
+
+    }
+
+    public Employer(String companyName, String email, String password) {
+        this.companyName = companyName;
+        this.email = email;
+        this.password = password;
+    }
+
+    
+
 }
