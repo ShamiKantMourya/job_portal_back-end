ca206a13e71d0c15d3875e5ecbf903125cec0efc
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-20 21:04:25 +0530

    employee constructor

--- src/main/java/com/jobportal/job_portal/entity/Employee.java
+++ src/main/java/com/jobportal/job_portal/entity/Employee.java
@@ -1,5 +1,31 @@
 package com.jobportal.job_portal.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Employee {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+
+    public Employee(){
+
+    }
+
+    public Employee(String name, String email, String password, String phone) {
+        super();
+        // this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
 }
