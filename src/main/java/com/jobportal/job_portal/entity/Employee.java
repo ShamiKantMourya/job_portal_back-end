2f164d583a6fd8fb1a9b1cf3146ababba98f9d39
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-20 21:08:12 +0530

    getter and setter function

--- src/main/java/com/jobportal/job_portal/entity/Employee.java
+++ src/main/java/com/jobportal/job_portal/entity/Employee.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 public class Employee {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,8 +15,7 @@ public class Employee {
     private String password;
     private String phone;
 
-
-    public Employee(){
+    public Employee() {
 
     }
 
@@ -26,6 +25,46 @@ public class Employee {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 }
