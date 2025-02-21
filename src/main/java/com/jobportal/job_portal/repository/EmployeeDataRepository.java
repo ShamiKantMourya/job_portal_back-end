70930c4b89e51b0db29f4dc285fd2c6c1b3382a8
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-21 19:15:10 +0530

    Employee data repository

--- src/main/java/com/jobportal/job_portal/repository/EmployeeDataRepository.java
+++ src/main/java/com/jobportal/job_portal/repository/EmployeeDataRepository.java
@@ -1,1 +1,9 @@
 package com.jobportal.job_portal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jobportal.job_portal.entity.Employee;
+
+public interface EmployeeDataRepository extends JpaRepository<Employee, Long> {
+    Employee findByEmail(String email);
+}
