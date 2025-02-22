e649ce79bacb87ff75be886b51c6008e233654fb
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-22 20:59:11 +0530

    findByLocation

--- src/main/java/com/jobportal/job_portal/repository/JobDataRepository.java
+++ src/main/java/com/jobportal/job_portal/repository/JobDataRepository.java
@@ -3,9 +3,12 @@ package com.jobportal.job_portal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jobportal.job_portal.entity.Job;
+
+import java.util.List;
 import java.util.Optional;
 
 
 public interface JobDataRepository extends JpaRepository<Job, Long>{
     Optional<Job> findById(Long id);
+    List<Job> findByJobLocation(String jobLocation);
 }
