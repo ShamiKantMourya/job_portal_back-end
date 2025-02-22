b4e3bd3f33a8d6bb7d56e0831c9f7c38fbad96b2
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-22 19:35:49 +0530

    entity annotation added

--- src/main/java/com/jobportal/job_portal/entity/Employer.java
+++ src/main/java/com/jobportal/job_portal/entity/Employer.java
@@ -1,9 +1,11 @@
 package com.jobportal.job_portal.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Employer {
 
     @Id
