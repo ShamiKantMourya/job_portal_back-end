fc426acd3b9f9c186b214ac95bfc89f03117ca07
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-22 21:11:29 +0530

    job controller

---
+++ src/main/java/com/jobportal/job_portal/controllers/JobController.java
@@ -0,0 +1,10 @@
+package com.jobportal.job_portal.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jobs")
+public class JobController {
+    
+}
