98e580cff34ae0f47899cabc16cac1a4df582f13
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-25 20:53:09 +0530

    employee service

--- src/main/java/com/jobportal/job_portal/service/EmployeeService.java
+++ src/main/java/com/jobportal/job_portal/service/EmployeeService.java
@@ -1,13 +1,37 @@
 package com.jobportal.job_portal.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jobportal.job_portal.entity.Employee;
 import com.jobportal.job_portal.repository.EmployeeDataRepository;
 
 @Service
 public class EmployeeService {
- 
+
     @Autowired
     private EmployeeDataRepository employeeDataRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeDataRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeDataRepository.findById(id);
+    }
+
+    public Employee creatEmployee(Employee employee) {
+        return employeeDataRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeDataRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeDataRepository.deleteById(id);
+    }
 }
