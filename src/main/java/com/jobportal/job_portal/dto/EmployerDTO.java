a1d0e281cdac2dc29ef2bf099cdf971fc58e167d
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-25 20:38:40 +0530

    employer dto file

--- src/main/java/com/jobportal/job_portal/dto/EmployerDTO.java
+++ src/main/java/com/jobportal/job_portal/dto/EmployerDTO.java
@@ -1,5 +1,49 @@
 package com.jobportal.job_portal.dto;
 
 public class EmployerDTO {
-    
+    private Long employerId;
+    private String companyName;
+    private String email;
+    private String phone;
+
+    public EmployerDTO() {}
+
+    public EmployerDTO(Long employerId, String companyName, String email, String phone) {
+        this.employerId = employerId;
+        this.companyName = companyName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getEmployerId() {
+        return employerId;
+    }
+
+    public void setEmployerId(Long employerId) {
+        this.employerId = employerId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
