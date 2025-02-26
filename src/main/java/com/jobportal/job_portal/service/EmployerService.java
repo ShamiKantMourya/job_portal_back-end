5062cca1cbce889b0ad32aa7e0d0a98509872f5e
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-26 20:34:36 +0530

    create and delete employer

--- src/main/java/com/jobportal/job_portal/service/EmployerService.java
+++ src/main/java/com/jobportal/job_portal/service/EmployerService.java
@@ -3,6 +3,7 @@ package com.jobportal.job_portal.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jobportal.job_portal.entity.Employer;
 import com.jobportal.job_portal.repository.EmployerDataRepository;
 
 @Service
@@ -10,5 +11,13 @@ public class EmployerService{
 
     @Autowired
     private EmployerDataRepository employerDataRepository;
+    
+    public Employer saveEmployer(Employer employer) {
+        return employerDataRepository.save(employer);
+    }
+
+    public void deleteEmployer(Long id) {
+        employerDataRepository.deleteById(id);
+    }
     
 }
