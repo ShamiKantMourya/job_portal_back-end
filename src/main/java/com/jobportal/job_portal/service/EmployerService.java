fb1487e4ba7991fa53c74cfe9897d3036980fb29
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-26 20:39:49 +0530

    remove unwanted code

--- src/main/java/com/jobportal/job_portal/service/EmployerService.java
+++ src/main/java/com/jobportal/job_portal/service/EmployerService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.jobportal.job_portal.entity.Employee;
 import com.jobportal.job_portal.entity.Employer;
 import com.jobportal.job_portal.repository.EmployerDataRepository;
 
