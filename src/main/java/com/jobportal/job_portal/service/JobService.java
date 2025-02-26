f3459dc99e15a76abf396653a1702ea598a81f89
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-26 21:17:36 +0530

    job service

--- src/main/java/com/jobportal/job_portal/service/JobService.java
+++ src/main/java/com/jobportal/job_portal/service/JobService.java
@@ -3,6 +3,7 @@ package com.jobportal.job_portal.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jobportal.job_portal.entity.Job;
 import com.jobportal.job_portal.repository.JobDataRepository;
 
 @Service
@@ -11,4 +12,19 @@ public class JobService {
     @Autowired
     private JobDataRepository jobDataRepository;
     
+    public Job createJob(Job job){
+        return jobDataRepository.save(job);
+    }
+
+    public Job getJobById(Long id){
+        return jobDataRepository.findById(id).get();
+    }
+
+    public Job updateJob(Job job){
+        return jobDataRepository.save(job);
+    }
+
+    public void deleteJob(Long id){
+        jobDataRepository.deleteById(id);
+    }
 }
