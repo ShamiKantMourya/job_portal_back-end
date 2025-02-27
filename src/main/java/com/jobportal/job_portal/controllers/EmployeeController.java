7862f68e0db515731f4af917744d712dfb84e369
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-27 21:15:32 +0530

    getEmployeeById api

--- src/main/java/com/jobportal/job_portal/controllers/EmployeeController.java
+++ src/main/java/com/jobportal/job_portal/controllers/EmployeeController.java
@@ -1,8 +1,11 @@
 package com.jobportal.job_portal.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +15,17 @@ import com.jobportal.job_portal.entity.Employee;
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
-    
+
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping
+    @GetMapping("/allEmployees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
     }
 }
