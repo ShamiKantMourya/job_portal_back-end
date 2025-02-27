5d3c79793300a677b7db52fcf34a29f7eefa2f9f
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-27 21:32:18 +0530

    get all employer api

--- src/main/java/com/jobportal/job_portal/controllers/EmployerController.java
+++ src/main/java/com/jobportal/job_portal/controllers/EmployerController.java
@@ -1,8 +1,23 @@
 package com.jobportal.job_portal.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import com.jobportal.job_portal.service.EmployerService;
+import com.jobportal.job_portal.entity.Employer;
+
 @RestController
+@RequestMapping("/api/employer")
 public class EmployerController {
     
+    @Autowired
+    private EmployerService employerService;
+
+    @GetMapping("/allEmployers")
+    public Iterable<Employer> getAllEmployers() {
+        return employerService.getAllEmployers();
+    }
 }
