6cd0313416a39bbb58fa5a86c43bd22feac79883
ShamiKantMourya <shamimourya9852@gmail.com>
2025-02-28 19:20:47 +0530

    delete employer

--- src/main/java/com/jobportal/job_portal/controllers/EmployerController.java
+++ src/main/java/com/jobportal/job_portal/controllers/EmployerController.java
@@ -3,8 +3,11 @@ package com.jobportal.job_portal.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,7 @@ import com.jobportal.job_portal.service.EmployerService;
 import com.jobportal.job_portal.entity.Employer;
 
 @RestController
-@RequestMapping("/api/employer")
+@RequestMapping("/api/v1/employer")
 public class EmployerController {
 
     @Autowired
@@ -29,7 +32,17 @@ public class EmployerController {
     }
 
     @PostMapping("/createEmployer")
-    public Employer createEmployer(Employer employer) {
+    public Employer createEmployer(@RequestBody Employer employer) {
         return employerService.createEmployer(employer);
+    }
+
+    @PostMapping("/updateEmployer/{id}")
+    public Employer updateEmployer(@PathVariable  Long id, @RequestBody Employer employer) {
+        return employerService.updateEmployer(employer);
+    }
+
+    @DeleteMapping("/deleteEmployer/{id}")
+    public void deleteEmployer(@PathVariable Long id) {
+        employerService.deleteEmployer(id);
     }
 }
