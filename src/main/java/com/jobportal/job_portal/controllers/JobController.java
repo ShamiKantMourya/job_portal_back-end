b41b61c7edefc6d492656743a64dea831015d5b1
ShamiKantMourya <shamimourya9852@gmail.com>
2025-03-01 20:47:49 +0530

    get all jobs method

--- src/main/java/com/jobportal/job_portal/controllers/JobController.java
+++ src/main/java/com/jobportal/job_portal/controllers/JobController.java
@@ -1,10 +1,27 @@
 package com.jobportal.job_portal.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jobportal.job_portal.entity.Job;
+import com.jobportal.job_portal.service.JobService;
 
 @RestController
 @RequestMapping("/api/jobs")
 public class JobController {
+    
+    @Autowired
+    private JobService jobService;
+
+    @GetMapping("/allJobs")
+    public Iterable<Job> getAllJobs() {
+        return jobService.getAllJobs();
+    }
+    @GetMapping("/job")
+    public Job getJobById(Long id) {
+        return jobService.getJobById(id);
+    }
     
 }
