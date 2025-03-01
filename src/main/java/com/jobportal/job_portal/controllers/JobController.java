9cbad40753ba3dd84c7a4042f982ffa2d34578c1
ShamiKantMourya <shamimourya9852@gmail.com>
2025-03-01 20:48:40 +0530

    create job method

--- src/main/java/com/jobportal/job_portal/controllers/JobController.java
+++ src/main/java/com/jobportal/job_portal/controllers/JobController.java
@@ -2,6 +2,7 @@ package com.jobportal.job_portal.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,9 @@ public class JobController {
     public Job getJobById(Long id) {
         return jobService.getJobById(id);
     }
-    
+  
+    @PostMapping("/createJob")
+    public Job createJob(Job job) {
+        return jobService.createJob(job);
+    }
 }
