1ed937f86452614bee74d5773d12b4f00f97e6d7
ShamiKantMourya <shamimourya9852@gmail.com>
2025-03-16 21:04:17 +0530

    code refactor

--- src/main/java/com/jobportal/job_portal/dto/EmployerDTO.java
+++ src/main/java/com/jobportal/job_portal/dto/EmployerDTO.java
@@ -44,7 +44,7 @@ public class EmployerDTO {
         this.phone = phone;
     }
 
-        public void setCompanyName(String companyName) {
+    public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
 }
